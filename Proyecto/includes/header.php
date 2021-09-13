e8e374eb5b8bc466795fbb2cffdc7b9f350e55ca
Samuel Cruz <74215505+SDark82@users.noreply.github.com>
2021-09-13 06:54:24 -0600

    Update header.php
    Se coloco un efecto hover en el nav del index

--- Proyecto/includes/header.php
+++ Proyecto/includes/header.php
@@ -9,6 +9,24 @@
     <link rel="shortcut icon" href="statics/img/Logo1.svg" type="image/x-icon">
     <link rel="stylesheet" href="statics/css/estilo.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet"> 
+    <style>
+        nav a {
+            font-family: Helvetica, sans-serif;
+            font-size: 25px;
+            height: 30px;
+            padding: 8px;
+            margin: 1px;
+            transition: 300ms all;
+            text-align: center;
+            text-decoration: none;
+            text-transform: uppercase;
+            line-height: 30px;
+        }
+        nav a:hover{
+            background-color: #fff;
+            color:  #7f3b8c ;
+        }
+    </style>
 </head>
 
 <body>
@@ -16,11 +34,10 @@
       <img src="static/img/Logo1.svg" alt="" />
       <img src="/Veterinaria/Proyecto/statics/img/Logo1.svg" alt="" style="width: 25%;padding-left: 10%;padding-top: 1%;" />
         <nav>
-          
-            <a href="index.php">Inicio </a>
-            <a href="historia.php">Historia</a>
-            <a href="modules/registro.php">Registrarse</a>
-            <a href="modules/login.php">Ingresar</a>
+            <a href="index.php" style="text-decoration: none;">Inicio </a>
+            <a href="historia.php" style="text-decoration: none;">Historia</a>
+            <a href="modules/registro.php" style="text-decoration: none;">Registrarse</a>
+            <a href="modules/login.php" style="text-decoration: none;">Ingresar</a>
         </nav>
         <div class="wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
                 style="height: 100%; width: 100%;">
