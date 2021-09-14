f6437c420476817df1b33b9dfbe16aed8b51bb1f
Samuel Cruz <74215505+SDark82@users.noreply.github.com>
2021-09-13 23:03:05 -0600

    Update admin_header.php

--- Proyecto/includes/admin_header.php
+++ Proyecto/includes/admin_header.php
@@ -7,16 +7,16 @@
         </div>
         <div class="navbar">
           <div class="content-navbar" onclick="location.href = '../index.php'">
-            <a href="../index.php">INICIO</a>
+            <a href="../../../index.php">INICIO</a>
           </div>
           <div class="content-navbar" onclick="">
             <a href="">PERFIL</a>
           </div>
           <div class="content-navbar" onclick="location.href = '/Proyecto/modules/administrador/administrador.php'">
-            <a href="../modules/administrador/administrador.php">ADMINISTRAR</a>
+            <a href="../administrador.php">ADMINISTRAR</a>
           </div>
           <div class="content-navbar" onclick="location.href = '/Proyecto/modules/login.php'">
-            <a href="../modules/login.php">SALIR</a>
+            <a href="../../../modules/login.php">SALIR</a>
           </div>
         </div>
 </header>
